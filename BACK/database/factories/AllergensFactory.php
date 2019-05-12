6db8895661fceca943fa9b77c33c5c0b1ecee25b
Simon Vos <simon.vos@hotmail.com>
2019-05-12 18:24:24 +0200

    commit before pull init database models

---
+++ BACK/database/factories/AllergensFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Allergens;
+use Faker\Generator as Faker;
+
+$factory->define(Allergens::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
